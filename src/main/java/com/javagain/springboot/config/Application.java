b6da1d1340e3c33c7e51b7ece753415c15a86d9c
sandeep sharma <sandeep.sharma@trantorinc.com>
2017-10-06 13:50:21 +0530

    Initial Commits

---
+++ src/main/java/com/javagain/springboot/config/Application.java
@@ -0,0 +1,11 @@
+package com.javagain.springboot.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+}
